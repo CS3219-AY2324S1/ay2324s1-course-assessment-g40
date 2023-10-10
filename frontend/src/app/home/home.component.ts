cd3bd9fc388fa85238a4d7330c807db2bb10c7e2
RX <R.@Richards-MacBook-Pro.local>
2023-10-10 22:25:03 +0800

    initial commit/not working yet

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { UserService } from '../_services/user.service';
 
 @Component({
@@ -8,7 +9,8 @@ import { UserService } from '../_services/user.service';
 })
 export class HomeComponent implements OnInit{
   content?: string;
-  constructor(private userService: UserService) { }
+  questionEndpoint: string = '';
+  constructor(private userService: UserService, private http: HttpClient) { }
 
   ngOnInit(): void {
     this.userService.getPublicContent().subscribe({
@@ -24,5 +26,22 @@ export class HomeComponent implements OnInit{
         }
       }
     });
+  }
+
+  onSubmit(): void {
+    const headers = {
+      'Content-Type': 'application/json',
+    };
+    // Send an HTTP GET request to your serverless function with the input as a parameter
+    this.http.get('http://localhost:8888/.netlify/functions/fetchQuestions', { headers }).subscribe(
+      (data: any) => {
+        // Handle the response from the serverless function here
+        console.log(data);
+      },
+      (error) => {
+        // Handle any errors here
+        console.error(error);
+      }
+    );
   }
 }
